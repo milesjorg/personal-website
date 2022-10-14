e2727f2838ef3d4989a4e4498a7f7649ba0471c1
Miles Jorgensen <55212275+milesjorg@users.noreply.github.com>
2022-10-13 21:32:32 -0700

    Correct typo

--- main.js
+++ main.js
@@ -2,19 +2,19 @@ $("body").terminal(
   function (command) {
     switch (command) {
       case "help":
-        this.echo(help, { typing: true, delay: 20 });
+        this.echo(help, { typing: true, delay: 30 });
         break;
       case "contact":
-        this.echo(contact, { typing: true, delay: 20 });
+        this.echo(contact, { typing: true, delay: 30 });
         break;
       case "aboutme":
-        this.echo(aboutme, { typing: true, delay: 20 });
+        this.echo(aboutme, { typing: true, delay: 10 });
         break;
       case "resume":
         this.echo($('<img src="resume.jpg">'));
         break;
       case "projects":
-        this.echo(projects, { typing: true, delay: 20 });
+        this.echo(projects, { typing: true, delay: 30 });
         break;
       case "Self Driving Car":
       case "self driving car":
@@ -55,7 +55,7 @@ aboutme =
   "\tjump into a new field and gained valuable experience as a Software QA Automation Engineer. This opened the door for my new career\n" +
   "\tpath where I went on to learn other object oriented programming languages like [[b;#ec03fc;none]Java], [[b;#ec03fc;none]Javascript], and [[b;#ec03fc;none]C#] through work experience \n" +
   "\tand personal projects. I am now ready to make the next jump in my career and I am excited to learn, grow, and develop new skills\n" +
-  "\tat a company that offers a role for growth as a [[b;#ec03fc;none]software developer].";
+  "\tat a company that offers a role for growth as a [[b;#ec03fc;none]software developer].\n";
 
 contact =
   "\t\t[[b;;]Miles Jorgensen]\n" +
@@ -70,20 +70,20 @@ projects =
 
 selfDrivingCar =
   "\t\t[[b;#df64ed;none]Self Driving Car -] [[bi;#df64ed;none]Machine Learning Project] \n" +
-  "\tCreated a simple webpage game where a car is controlled to weave through traffic. Implemented a machine learning \n" +
-  "\tneural network to control the car and learn how to drive it. The network is visualized next to the game where\n" +
-  "\tthe inputs, outputs, and hidden layers show the activity of the best performing car. The amount of traffic and\n" +
-  "\tcharacteristics of the car can be controlled from inputs given by the user to see how the behavior changes with\n" +
-  "\tthe given input values. Users can save the 'brain' of the highest performing car of the generation and use that\n" +
-  "\tas a starting point for the next generation which are mutated versions of that 'brain'. Try it yourself [[!;;;;https://milesjorg.netlify.app/self-driving-car/self-driving-car.html]here]\n";
+  "\tCreated a simple webpage game where a car is controlled to weave through traffic. Implemented a neural network \n" +
+  "\tto control the car and learn how to drive it. The network is visualized next to the game where the inputs, \n" +
+  "\toutputs, and hidden layers show the activity of the best performing car. The amount of traffic and characteristics\n" +
+  "\tof the car can be controlled from inputs given by the user to see how the behavior changes with the given input \n" +
+  "\tvalues. Users can save the 'brain' of the highest performing car of the generation and use that as a starting point \n" +
+  "\tfor the next generation which are mutated versions of that 'brain'. Try it yourself [[!;;;;https://milesjorg.netlify.app/self-driving-car/self-driving-car.html]here]\n";
 
 markovText =
   "\t\t[[b;#df64ed;none]Markov Text Generator -] [[bi;#df64ed;none]Markov Chains and Webscraping] \n" +
   "\tImplemented Markov chains to generate 'prose' using the Markovify library. Text was provided the Project Gutenberg \n" +
-  "\tlibrary from ntlk.corpus package. In addition, I implemented webscraping with the Bueatiful Soup library to take \n" +
+  "\tlibrary from ntlk.corpus package. In addition, I implemented webscraping with the Beautiful Soup library to take \n" +
   "\tsong lyrics from the top ten songs section of https://genius.com/\n" +
   "\tHere are some sample quotes made from Walt Whitmans 'Leaves in the Grass' and the top songs from genius.com:\n" +
   "\tCan you tell which quotes came from which text?\n\n" +
   "\t\tAre you faithful to all, He and the bells of the earth, it is eternal, \n\t\tThe law of heroes and the thousand island paradises of the body, if not more.\n\n" +
   "\t\tAnd all or my words itch at the lawyer plead for my city, \n\t\tWhere speculations on their old Thames, I only to face!\n\n" +
-  "\t\tNotti Like what? \n\t\tThey mans got the deal that it feels? \n\t\t41 K but I want the what? \n\t\tYeah yeah yo Oh tell me we ain't playing no defense Like what?";
+  "\t\tNotti Like what? \n\t\tThey mans got the deal that it feels? \n\t\t41 K but I want the what? \n\t\tYeah yeah yo Oh tell me we ain't playing no defense Like what?\n";
